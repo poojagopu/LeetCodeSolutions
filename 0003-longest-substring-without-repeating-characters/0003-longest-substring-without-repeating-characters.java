dcfa1846afe347367e8a7d1240e2b1c539d050a6
poojagopu <60793685+poojagopu@users.noreply.github.com>
2023-01-03 05:13:56 -0500

    Time: 15 ms (46.57%), Space: 46.8 MB (16.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,9 +1,8 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character>set=new HashSet<>();
-        int max=0;
+        HashSet<Character> set=new HashSet<>();
+        int j=0; int max=0;
         int i=0;
-        int j=0;
         while(i<s.length()){
             if(!set.contains(s.charAt(i))){
                 set.add(s.charAt(i));
@@ -15,6 +14,7 @@ class Solution {
             }
         }
         return max;
-
+        
+        
     }
 }
