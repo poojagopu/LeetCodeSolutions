93069b74d0472c25a8d469d051c7d4380ba4aceb
poojagopu <60793685+poojagopu@users.noreply.github.com>
2023-02-14 07:47:36 -0500

    Time: 1 ms (100.00%), Space: 43.6 MB (78.86%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        int start=0;
+        while(i<nums.length){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[start];
+                nums[start]=temp;
+                start++;
+            }
+            i++;
+        }
+    }
+}
